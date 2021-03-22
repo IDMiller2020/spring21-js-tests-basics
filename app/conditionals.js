551f425b3d36dcc5e54cf10e4a38e9419411b6f2
IDMiller2020 <psiauto@gmail.com>
2021-03-22 15:23:15 -0600

    Conditionals completed

--- app/conditionals.js
+++ app/conditionals.js
@@ -2,7 +2,11 @@
 
 function largerNum(num1, num2) {
     // you code here
-
+  if (num1 > num2) {
+      return num1
+  } else {
+      return num2
+  }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,7 +18,17 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+  if (score/possible < 0.60) {
+      return 'F'
+  } else if (score/possible < 0.7) {
+      return 'D'
+  } else if (score/possible < 0.8) {
+      return 'C'
+  } else if (score/possible < 0.9) {
+      return 'B'
+  } else {
+      return 'A'
+  }
 }
 
 
@@ -30,12 +44,26 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
-
+  if (hour >= 5 && hour <= 11) {
+      return 'good morning'
+  } else if (hour >= 12 && hour <= 17) {
+      return 'good afternoon'
+  } else if (hour >= 18 && hour <= 21) {
+      return 'good evening'
+  } else {
+      return 'good night'
+  }
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+  if (temp >= 103) {
+      return 'fever go to hospital'
+  } else if (temp > 98.6) {
+      return 'fever'
+  } else {
+      return 'no fever'
+  }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +75,7 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+  return !car.moving
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +86,9 @@ let dish = {
 }
 
 function washDish(dish) {
-
+  if (dish.yourDish && dish.isDirty) {
+      return true
+  } else {
+      return false
+  }
 }
